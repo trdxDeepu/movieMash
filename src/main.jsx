dac3c204e7c535ee1dcf4b639e24494e5991563a
trdxlucifer <singhdepu566@gmail.com>
2023-09-04 14:10:33 +0530

    changed the color of stars

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// import App from './App.jsx'
+// import './index.css'
 import StarRating from './StarRating.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5}/>
-    <StarRating maxRating={10}/>
-    <StarRating maxRating={20}/>
+    <StarRating maxRating={9}/>
+    
   </React.StrictMode>,
 )
