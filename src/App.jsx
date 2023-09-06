1debe0d8fee0a20db13472e400917105fea64e09
trdxlucifer <singhdepu566@gmail.com>
2023-09-06 22:24:52 +0530

    changed useEffect to search

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Navbar, Logo, NumResult } from "./components/Navbar";
 import Box from "./components/Box";
 import MainItem from "./components/MainItem";
@@ -9,75 +9,28 @@ import { WatchedMovieList, WatchedMovie } from "./components/WatchedMovieList";
 import Summary from "./components/Summary";
 import Search from "./components/Search";
 
-const tempMovieData = [
-  {
-    imdbID: "tt1375666",
-    Title: "Inception",
-    Year: "2010",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt0133093",
-    Title: "The Matrix",
-    Year: "1999",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt6751668",
-    Title: "Parasite",
-    Year: "2019",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
-  },
-];
-
-const tempWatchedData = [
-  {
-    imdbID: "tt1375666",
-    Title: "Inception",
-    Year: "2010",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-    runtime: 148,
-    imdbRating: 8.8,
-    userRating: 10,
-  },
-  {
-    imdbID: "tt0088763",
-    Title: "Back to the Future",
-    Year: "1985",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-    runtime: 116,
-    imdbRating: 8.5,
-    userRating: 9,
-  },
-];
-
 
 const tempQuery = "Mission Impossible";
 
 export default function App() {
- 
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
 
-  
-
   return (
     <>
       <Navbar>
         <Logo />
-        <Search setMovies={setMovies} setError={setError} setLoading={setLoading} />
+        <Search
+          setMovies={setMovies}
+          setError={setError}
+          setLoading={setLoading}
+        />
         <NumResult movies={movies} />
       </Navbar>
       <MainItem>
         <Box>
-          {/* {loading ? <Loader /> : <MovieList movies={movies} />} */}
           {loading && <Loader />}
           {!loading && !error && <MovieList movies={movies} />}
           {error && <ErrorMessage message={error} />}
